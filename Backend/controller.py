36939155275e69957f00bb0dba0d03a68d192924
Andres Gutierrez <andygtz675@gmail.com>
2019-10-07 15:51:52 -0500

    fixed relative paths for controller

--- Backend/controller.py
+++ Backend/controller.py
@@ -1,7 +1,7 @@
 from flask_restful import Resource, Api
 from flask import Flask
 from src import REST
-from src.controller.helloWorldController import HelloWorldController
+from src.controller import *
 
 api = Api(REST)
 
