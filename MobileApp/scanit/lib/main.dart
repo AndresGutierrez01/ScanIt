08a2f002bc3c9c3b982cae17908ba566728a0008
Tomas Torres <tomas3torres@live.com>
2019-10-07 01:29:54 -0500

    created login page

--- MobileApp/scanit/lib/main.dart
+++ MobileApp/scanit/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'pages/Home.dart';
+import 'pages/Login.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Home(),
+      home: Login(),
     );
   }
 }
