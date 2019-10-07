b40ae88e2a3835fd5a917daaca1aeb8f865a3a81
Tomas Torres <tomas3torres@live.com>
2019-10-07 01:49:39 -0500

    added AppColors class

---
+++ MobileApp/scanit/lib/utilites/AppColors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color green = const Color(0xFF99F280);
+  static Color yellow = const Color(0xFFDAF222);
+  static Color aqua = const Color(0xFF41D9BD);
+  static Color greenAccent = const Color(0xFFBDF249);
+  static Color blue = const Color(0xFF0FDBF2);
+  static Color white = const Color(0xFFFFFFFF);
+}
