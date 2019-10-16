4853867581293a96f955dca85fb19d10a4e47f8d
Tomas Torres <tomas3torres@live.com>
2019-10-16 18:17:24 -0500

    Created tab view for class page

--- MobileApp/scanit/lib/pages/Class.dart
+++ MobileApp/scanit/lib/pages/Class.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:scanit/utilites/AppColors.dart';
+import 'package:scanit/widgets/FormButton.dart';
 
 
 class Class extends StatefulWidget {
@@ -18,13 +19,51 @@ class Class extends StatefulWidget {
 class _ClassState extends State<Class> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Container(
-        color: AppColors.background,
-        child: Center(child: Text(widget.id),),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: TabBar(
+            indicatorColor: AppColors.aqua,
+            tabs: <Widget>[
+              Tab(text: "Tests"),
+              Tab(text: "Students",)
+            ],
+          ),
+          title: Text(widget.title, style: TextStyle(color: AppColors.aqua),),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(30),
+              color: AppColors.background,
+               child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(""),
+                  FormButton(
+                    text: ("Create Test"),
+                    onTap: ()=>{},
+                  )
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(30),
+              color: AppColors.background,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(""),
+                  FormButton(
+                    text: ("Add Student"),
+                    onTap: ()=>{},
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
