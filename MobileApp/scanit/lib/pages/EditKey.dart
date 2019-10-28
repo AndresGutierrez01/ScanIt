42737cb32ce089693a11c93c88874d2461854ca8
Tomas Torres <tomas3torres@live.com>
2019-10-27 21:09:23 -0500

    added edit key page

---
+++ MobileApp/scanit/lib/pages/EditKey.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:scanit/utilites/AppColors.dart';
+
+class EditKey extends StatefulWidget {
+  final String testId;
+  final String classId;
+
+  EditKey({@required this.testId, @required this.classId});
+
+  @override
+  _EditKeyState createState() => _EditKeyState();
+}
+
+class _EditKeyState extends State<EditKey> {
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+    );
+  }
+}
--- MobileApp/scanit/lib/pages/Test.dart
+++ MobileApp/scanit/lib/pages/Test.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:scanit/pages/EditKey.dart';
+import 'package:scanit/utilites/AppColors.dart';
+import 'package:scanit/widgets/SlideRightRoute.dart';
 
 class Test extends StatefulWidget {
   final String testId;
   final String classId;
   final String name;
 
-  Test({
-    @required this.testId,
-    @required this.classId,
-    @required this.name
-  });
+  Test({@required this.testId, @required this.classId, @required this.name});
 
   @override
   _TestState createState() => _TestState();
@@ -18,9 +17,25 @@ class Test extends StatefulWidget {
 class _TestState extends State<Test> {
   @override
   Widget build(BuildContext context) {
+    editKey() {
+      Navigator.of(context).push(
+        SlideRightRoute(
+            widget: EditKey(classId: widget.classId, testId: widget.testId)),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.name),
+        actions: <Widget>[
+          IconButton(
+            onPressed: editKey,
+            icon: Icon(
+              Icons.vpn_key,
+              color: AppColors.white,
+            ),
+          )
+        ],
       ),
     );
   }
