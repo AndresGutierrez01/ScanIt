e5a75cdbc47906ebab96234a7911c1171fc22747
Andres Gutierrez <andygtz675@gmail.com>
2019-10-31 07:46:16 -0500

    get the bubbles for each column

--- Backend/src/scanner_service.py
+++ Backend/src/scanner_service.py
@@ -22,15 +22,15 @@ class ScannerService:
     def __init__(self, image_file):
         self.scantron = self.get_scantron()
         # self.cnts = self.get_contours()
-        # self.scantron_questions = self.get_scantron_questions()
-        self.get_columns()
+        self.scantron_questions = self.get_scantron_questions()
 
     def get_scantron(self):
 
         image_path = os.path.join(
-            os.getcwd(), 'scantron_images', 'Bubble_Sheet_pic_13.jpg')
+            os.getcwd(), 'scantron_images', 'Bubble_Sheet_pic_15.jpg')
         image = cv2.imread(image_path)
 
+        # grayscale the image
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
         # apply Otsu's thresholding method to binarize the warped
@@ -88,7 +88,7 @@ class ScannerService:
 
         return scantron
 
-    def get_columns(self):
+    def get_column_contours(self):
 
         # dilate the scantron
         # this will make the lines more prominent and remove
@@ -114,8 +114,8 @@ class ScannerService:
             # if our approximated contour has four points
             # and if it matches the aspect ratio we are looking for
             # then we can assume we have found a column
-            if len(approx) == 4 and ar < .5:
-                colCnts.append(c)
+            if len(approx) == 4 and ar < .6:
+                colCnts.append(approx)
 
         # sort the contours from left to right
         colCnts = contours.sort_contours(colCnts,
@@ -123,38 +123,88 @@ class ScannerService:
 
         return colCnts
 
-    def get_contours(self):
-        # apply Otsu's thresholding method to binarize the warped
-        # piece of paper
-        self.thresh = cv2.threshold(self.image, 0, 255,
-                                    cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+    def get_scantron_bubbles(self, col):
+        # dilate the bubbles in the column
+        # this will make the lines more prominent and remove
+        # imperfections in the contours
+        kernel = np.ones((2, 2), np.uint8)
+        dilated = cv2.dilate(col, kernel, iterations=2)
 
-        # find contours in the thresholded image, then initialize
-        # the list of contours that correspond to questions
-        cnts, heirarchy = cv2.findContours(self.thresh.copy(), cv2.RETR_LIST,
-                                           cv2.CHAIN_APPROX_SIMPLE)
+        labels = cv2.connectedComponentsWithStats(dilated)[1]
+        np.zeros((dilated.shape[0], dilated.shape[1], 3), np.uint8)
 
-        return cnts
+        foundCnts = []
+        for i in range(0, labels.max()+1):
+            mask = cv2.compare(labels, i, cv2.CMP_EQ)
+
+            cnts, _ = cv2.findContours(
+                mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+            for c in cnts:
+                # compute the bounding box of the contour, then use the
+                # bounding box to derive the aspect ratio
+                (x, y, w, h) = cv2.boundingRect(c)
+                ar = w / float(h)
+
+                # check if aspect ration of bubbles
+                # are within acceptable range
+                if ar >= 0.8 and ar <= 1.2:
+                    foundCnts.append(c)
+
+        foundCnts = sorted(foundCnts, key=cv2.contourArea, reverse=True)
+
+        scantronCnts = []
+
+        # initialize the width that is expected of a santron bubble
+        expectedWidth = None
+        for c in foundCnts:
+
+            (x, y, w, h) = cv2.boundingRect(c)
+            ar = w / float(h)
+
+            # initialize the width that is expected of a santron bubble
+            if expectedWidth == None:
+                expectedWidth = w
+
+            # if the current width is out of the accepted range then
+            # do not add to scantron contours
+            if abs(w - expectedWidth) > 4:
+                continue
+
+            scantronCnts.append(c)
+
+        return scantronCnts
 
     def get_scantron_questions(self):
-        columns = self.get_columns()
+
+        # get the individual column contours from the scantron
+        columnCnts = self.get_column_contours()
 
         scantron_questions = []
 
         # loop through the columns in the scantron and
         # find their respective bubbles
-        for col in columns:
-            col_bubbles = self.get_scantron_bubbles(col)
+        for col in columnCnts:
+
+            # apply a four point perspective transform to both the
+            # original scantron and threshold to obtain a top-down
+            # birds eye view of the column
+            cur_col = four_point_transform(self.scantron, col.reshape(4, 2))
+            cur_col_color = four_point_transform(
+                self.scantron_color, col.reshape(4, 2))
+
+            col_bubbles = self.get_scantron_bubbles(cur_col)
 
             # sort the bubbles from top to bottom
             col_bubbles = contours.sort_contours(col_bubbles,
                                                  method="top-to-bottom")[0]
 
-            # remove the extra inner-bubble
-            # this is to fix the problem with cv2.RETR_EXTERNAL
             for c in col_bubbles:
-                if cv2.boundingRect(c)[2] > 45:
-                    scantron_questions.append(c)
+                cv2.drawContours(cur_col_color, [c], -1, self.colors[1])
+
+            cv2.imshow('image', cur_col_color)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
 
         return scantron_questions
 
@@ -213,32 +263,6 @@ class ScannerService:
             student_id.append(bubbled[1])
 
         return ''.join([str(s) for s in student_id[:id_len]])
-
-    def get_scantron_bubbles(self, bounding_box):
-
-        questionCnts = []
-
-        (box_x, box_y, box_w, box_h) = cv2.boundingRect(bounding_box)
-
-        left_bound = box_x
-        right_bound = box_x + box_w
-
-        # loop over the contours
-        for c in self.cnts:
-            # compute the bounding box of the contour, then use the
-            # bounding box to derive the aspect ratio
-            (x, y, w, h) = cv2.boundingRect(c)
-            ar = w / float(h)
-
-            # in order to label the contour as a question, region
-            # should be sufficiently wide, sufficiently tall, and
-            # have an aspect ratio approximately equal to 1
-            if w >= 20 and h >= 20 and ar >= 0.9 and ar <= 1.1:
-
-                if x > left_bound and x+w < right_bound:
-                    questionCnts.append(c)
-
-        return questionCnts
 
     def grade_test(self, key):
 
